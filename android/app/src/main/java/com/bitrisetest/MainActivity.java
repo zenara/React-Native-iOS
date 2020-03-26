da38de13d99f6c50a213f0ca8e80b80cdd408e8c
Chathurika Senani <senani@omobio.net>
2020-03-26 10:40:24 +0530

    Replace code

--- android/app/src/main/java/com/bitrisetest/MainActivity.java
+++ android/app/src/main/java/com/bitrisetest/MainActivity.java
@@ -1,4 +1,4 @@
-package com.firstproject_ios;
+package com.bitrisetest;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "FirstProject_ios";
+        return "bitriseTest";
     }
 }
